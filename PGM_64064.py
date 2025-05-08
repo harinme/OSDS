9a6fc713c532d440437e76beaca51c363df9c3ed
JW0304 <jwyang34@gmail.com>
2025-05-09 07:16:39 +0900

    250509

--- PGM_64064.py
+++ PGM_64064.py
@@ -1,10 +1,16 @@
-import re
+# 제재 아이디 목록의 경우의 수
+
+# 순열 불러오기
 from itertools import permutations
 
+# 제재 아이디가 유저 아이디와 일치하는지 확인
 def is_match(banned, user):
+    # 길이가 다른 경우
     if len(banned) != len(user):
         return False
+    # 제재 아이디와 유저 아이디의 튜플
     for b, u in zip(banned, user):
+        # 제재 아이디가 문자이고 유저 아이디와 다를 경우
         if b != '*' and b != u:
             return False
     return True
@@ -12,8 +18,21 @@ def is_match(banned, user):
 def solution(user_id, banned_id):
     result = set()
 
+    # 유저 아이디에서 제재 아이디 개수만큼 원소를 고른 모든 순열
     for case in permutations(user_id, len(banned_id)):
-        if all(is_match(b, u) for b, u in zip(banned_id, case)):
+        # 모두 일치하는지 확인
+        all_matched = True
+        # 하나의 제재 아이디, 유저 아이디
+        for i in range(len(banned_id)):
+            banned = banned_id[i]
+            user = case[i]
+            # 일치하지 않는 경우
+            if not is_match(banned, user):
+                all_matched = False
+                break
+        # 일치하는 경우
+        if all_matched:
+            # 조합 안에 조합 넣기
             result.add(frozenset(case))
 
     return len(result)
