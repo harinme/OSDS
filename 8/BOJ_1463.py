f7d4a14a7509065d2955def371c2c87fd4d64ab1
bonghyerin <bongko98@g.skku.edu>
2025-08-22 10:57:09 +0900

    dp추가

--- 8/BOJ_1463.py
+++ 8/BOJ_1463.py
@@ -50,3 +50,40 @@ def solve():
 k = int(input())
 ans = solve()
 print(ans)
+
+
+# DP 풀이
+
+# 거꾸로 이전 값에서 1 더하고, 2 곱하고, 3 곱하는 형태
+# 예 ) 6의 경우, 5에서 1을 더하고, 3에서 2를 곱하고, 2에서 3을 곱함
+# 여기서 계산 횟수가 가장 적은 값을 dp 테이블에 작성한다 
+
+# 테이블 정의 dp = [0] * (N+1)
+# 점화식 정의 dp[i] = min(dp[i-1]+1, dp[i//2]+1, dp[i//3]+1)
+
+
+import sys
+sys.stdin = open("1463_input.txt", "r")
+
+N = int(input())
+
+# DP 테이블 정의
+dp = [0] * (N + 1)
+
+# dp[1] = 0 초기값 설정
+
+for i in range(2, N + 1):
+    # 이전 값에서 +1 한 경우
+    dp[i] = dp[i-1] + 1
+
+    # dp[i] 가 2의 배수인 경우
+    if dp[i] % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2] + 1)
+    
+    # dp[i]가 3의 배수인 경우
+    if dp[i] % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
+    
+    
+ans = dp[N]
+print(ans)
